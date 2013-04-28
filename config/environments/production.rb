d4d71df4b40ffb57bd34d0b54523aed2825e93c3
Toshiaki <koshiba+github@4038nullpointer.com>
2013-04-28 23:18:05 +0900

    write log to  STDOUT
    https://devcenter.heroku.com/articles/logging#writing-to-your-log

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,13 +44,13 @@ Tweeffy::Application.configure do
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
+  config.logger = Logger.new(STDOUT)
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
